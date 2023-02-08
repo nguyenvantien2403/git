9ab5274acb77ad4d9a52e5f2c33edec53686fd3c
nguyenvantien2403 <88313441+nguyenvantien2403@users.noreply.github.com>
2023-02-09 01:02:31 +0700

    commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProcessDatabese pd = new ProcessDatabese();
+        SqlConnection connect;//khai bao doi tuong connnection
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = pd.ReadTable("select * from Table_1");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/ProcessDatabese.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    internal class ProcessDatabese
+    {
+        SqlConnection conn;
+        string s = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\SOURCE\\C#\\DEMOKTRA\\WindowsFormsApp1\\WindowsFormsApp1\\Database1.mdf;Integrated Security=True";
+
+        public ProcessDatabese()
+        {
+            conn = new SqlConnection(s);
+        }
+        private void Connect() // mo ket noi
+        {
+            if(conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+            }
+        }
+        private void Disconnect()  // dong ket noi
+        {
+            if(conn.State != ConnectionState.Closed)
+            {
+                conn.Close();
+            }
+        }
+        public DataTable ReadTable(string sql)
+        {
+
+            DataTable tb = new DataTable();
+            Connect();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(tb);
+            Disconnect();
+            tb.Dispose();
+            return tb;
+
+        }
+        public void RunSql(string sql)
+        {
+            Connect();
+            SqlCommand cm = new SqlCommand(sql, conn);
+            cm.ExecuteNonQuery();
+            Disconnect();
+        }
+    }
+}
